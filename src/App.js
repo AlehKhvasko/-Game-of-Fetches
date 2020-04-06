6e08e929d761fe5aed310dff8f765dfb2d95429f
Aleh <user@iMac-User.fios-router.home>
2020-04-06 01:27:50 -0400

    finished styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import axios from 'axios'
+import Output from './components/Output'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    constructor(props) {
+    super(props);
+
+        this.state = {
+            name: [],
+            region: [],
+            coat: [],
+            seat: [],
+            aliases: [],
+            founder: [],
+            founderName: [],
+            books: []
+        }
+    }
+componentDidMount() {
+    axios.all([
+    axios.get("http://anapioficeandfire.com/api/characters/16"),
+    axios.get("http://www.anapioficeandfire.com/api/houses/378"),
+    axios.get("http://www.anapioficeandfire.com/api/houses/229"),
+    axios.get("http://www.anapioficeandfire.com/api/houses/17"),
+    axios.get("http://www.anapioficeandfire.com/api/characters/901"),
+])
+    .then(axios.spread((name,region, coat, seat, aliases) => {
+        this.setState({name: name.data});
+        this.setState({region: region.data});
+        this.setState({coat: coat.data});
+        this.setState({seat: seat.data.seats[1]});
+        this.setState({aliases: aliases.data.aliases[1]});
+    }))
+    .catch(error => {
+        console.log(error);
+    })
+
+    fetch("http://www.anapioficeandfire.com/api/houses/362")
+        .then(res => {
+           return res.json()
+        })
+        .then(data => {
+            return data.founder;
+        })
+        .then(data => {
+            return fetch(data);
+        })
+        .then(result => {
+            return result.json()
+         })
+        .then(founder => {
+            this.setState({founder: founder.name})
+        }) 
+
+        axios.get('http://www.anapioficeandfire.com/api/characters/232')
+        .then(result => {
+            return result.data.povBooks
+        })
+        .then((data) => {
+                    data.map(result => {
+                    return axios.get(result)
+                    .then(name => {
+                         console.log(name.data.name);
+                         this.setState({books: name.data.name});
+                    })
+                })
+        });
+
+
 }
-
-export default App;
+render() {
+    return (
+        <div>
+          <Output name={this.state.name.born} />
+          <Output name={this.state.region.region} />
+          <Output name={this.state.coat.coatOfArms} />
+          <Output name={this.state.seat} />
+          <Output name={this.state.aliases} />
+          <Output name={this.state.founder} />
+          <Output name={this.state.books} />
+        </div>
+    )
+}
+}
---
+++ src/components/Output.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Output(props) {
+    return (
+        <div>
+            <p className='output'>{props.name}</p>
+        </div>
+    )
+}
+
+export default Output;
+
